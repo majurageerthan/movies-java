65ece1f81d22e3fe0e5434826ee10ab8e1f36b08
majurageerthan <majuran.thasan@gmail.com>
2024-02-14 22:55:56 +1100

    Refactored update method

--- src/main/java/com/majuran/movies/service/MovieService.java
+++ src/main/java/com/majuran/movies/service/MovieService.java
@@ -2,12 +2,16 @@ package com.majuran.movies.service;
 
 import com.majuran.movies.dto.MovieDto;
 import com.majuran.movies.model.Movie;
+import com.majuran.movies.model.Slot;
 import com.majuran.movies.repository.MovieRepository;
+import com.majuran.movies.repository.SlotRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static com.majuran.movies.util.DateUtil.getLocalDate;
 
@@ -16,6 +20,8 @@ import static com.majuran.movies.util.DateUtil.getLocalDate;
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final SlotRepository slotRepository;
+
 
     //    @Cacheable("movies")
     public List<MovieDto> getAllMovies(String date, String screenType) {
@@ -45,10 +51,29 @@ public class MovieService {
     //    @CacheEvict(value = "movies", key = "#id")
     public void updateMovie(Long id, MovieDto movieDto) {
         movieRepository.findById(id).ifPresent(existingMovie -> {
+            removeExistingSlots(existingMovie);
             existingMovie.setName(movieDto.getName());
             existingMovie.setScreen(movieDto.getScreen());
+            existingMovie.setSlots(getSlots(movieDto, existingMovie));
             movieRepository.save(existingMovie);
         });
     }
 
+    private List<Slot> getSlots(MovieDto movieDto, Movie existingMovie) {
+        return movieDto.getSlots().stream().map(slotDto -> {
+            Slot slot = new Slot();
+            slot.setMovie(existingMovie);
+            slot.setDate(slotDto.getDate());
+            slot.setTime(slotDto.getTime());
+            return slot;
+        }).collect(Collectors.toList());
+    }
+
+    private void removeExistingSlots(Movie movie) {
+        List<Slot> existingSlots = movie.getSlots();
+        existingSlots.forEach(slot -> slot.setMovie(null));
+        slotRepository.deleteAll(existingSlots);
+        movie.setSlots(new ArrayList<>());
+        movieRepository.save(movie);
+    }
 }
